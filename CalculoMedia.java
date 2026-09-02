0e64a65069c82bb366c136d1658b48161718110c
FelipeGit-codes <andre.f.santos8@ba.estudante.senai.br>
2026-09-02 16:00:53 -0300

    K

---
+++ CalculoMedia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculoMedia {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        System.out.print("Digite o terceiro número: ");
+        double numero3 = scanner.nextDouble();
+
+        double media = (numero1 + numero2 + numero3) / 3.0;
+
+        System.out.println("A média aritmética é: " + media);
+
+        scanner.close();
+    }
+}
